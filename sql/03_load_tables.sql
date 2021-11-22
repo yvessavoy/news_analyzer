e533790445e1e5031cc0b4d04a4c6706732a1cda
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2021-11-22 16:24:17 +0100

    Adapt SQL to include new 'comments'-table

--- sql/03_load_tables.sql
+++ sql/03_load_tables.sql
@@ -1,5 +1,12 @@
 USE news_analyzer;
 
-INSERT INTO SITE(ID, NAME) VALUES (1, '20 Minuten');
-INSERT INTO SITE(ID, NAME) VALUES (2, 'Blick');
-INSERT INTO SITE(ID, NAME) VALUES (3, 'Tagesanzeiger');
+INSERT INTO SITE(ID, NAME)
+VALUES
+    (1, '20 Minuten'),
+    (2, 'Blick'),
+    (3, 'Tagesanzeiger');
+
+INSERT INTO USER_TYPE (ID, TYPE)
+VALUES
+    (1, 'Author'),
+    (2, 'Commenter');
--- sql/04_create_users.sql
+++ sql/04_create_users.sql
@@ -6,22 +6,26 @@
 -- Create a user for the data aggregation scripts
 -- that has the right to SELECT and INSERT on all
 -- tables
+DROP USER 'data_retrieval'@'localhost';
 CREATE USER 'data_retrieval'@'localhost';
-GRANT SELECT ON news_analyzer.article TO 'data_retrieval'@'localhost';
-GRANT INSERT ON news_analyzer.article TO 'data_retrieval'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.ARTICLE TO 'data_retrieval'@'localhost';
+GRANT INSERT ON NEWS_ANALYZER.ARTICLE TO 'data_retrieval'@'localhost';
 
-GRANT SELECT ON news_analyzer.author TO 'data_retrieval'@'localhost';
-GRANT INSERT ON news_analyzer.author TO 'data_retrieval'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.USER TO 'data_retrieval'@'localhost';
+GRANT INSERT ON NEWS_ANALYZER.USER TO 'data_retrieval'@'localhost';
 
-GRANT SELECT ON news_analyzer.category TO 'data_retrieval'@'localhost';
-GRANT INSERT ON news_analyzer.category TO 'data_retrieval'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.CATEGORY TO 'data_retrieval'@'localhost';
+GRANT INSERT ON NEWS_ANALYZER.CATEGORY TO 'data_retrieval'@'localhost';
 
 -- Create a user for the data analysis / report generation script
 -- which only has the right to SELECT on all views and tables
+DROP USER 'report'@'localhost';
 CREATE USER 'report'@'localhost';
-GRANT SELECT ON news_analyzer.article TO 'report'@'localhost';
-GRANT SELECT ON news_analyzer.author TO 'report'@'localhost';
-GRANT SELECT ON news_analyzer.category TO 'report'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.ARTICLE TO 'report'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.USER TO 'report'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.CATEGORY TO 'report'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.SITE TO 'report'@'localhost';
+GRANT SELECT ON NEWS_ANALYZER.COMMENT TO 'report'@'localhost';
 
 -- Flush privileges to make them active immediately
 FLUSH PRIVILEGES;
