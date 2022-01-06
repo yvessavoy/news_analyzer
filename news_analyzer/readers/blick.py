db4328df6882fc39fefd20a6a0d3bf9ca1239260
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2022-01-06 14:54:06 +0100

    Adapt blick-reader to new parser structure

--- news_analyzer/readers/blick.py
+++ news_analyzer/readers/blick.py
@@ -6,6 +6,8 @@ import feedparser
 import requests
 
 from news_analyzer.database import Database
+from news_analyzer.database.models import Article, User
+from news_analyzer.readers import Reader
 
 feeds = [
     ('https://www.blick.ch/rss.xml', 'News'),
@@ -26,58 +28,49 @@ feeds = [
 ]
 
 
-def get_detail_html(url):
-    return requests.get(url).text
+class Blick(Reader):
+    def get_comment_count(self, article_id):
+        r = requests.get(
+            f'https://community.ws.blick.ch/community/comment?page=1&discussion_type_id={article_id}')
+        return r.json()['number_of_comments']
 
+    def get_article_length(self, html):
+        matches = re.findall('"articleLength":([0-9]+),', html)
+        if len(matches) > 0:
+            return int(matches[0])
+        else:
+            return 0
 
-def get_comment_count(article_id):
-    r = requests.get(
-        f'https://community.ws.blick.ch/community/comment?page=1&discussion_type_id={article_id}')
-    return r.json()['number_of_comments']
+    def process_entry(self, entry, *args):
+        category = args[0]
+        article = Article(site_id=2)
+        article.category_id = self.db.save_category(category)
+        article.title = entry.title
+        article.published_at = entry.published_parsed
+        article.external_id = entry.link.split('-id')[1].split('.')[0]
+        article.comment_count = self.get_comment_count(article.external_id)
+        article.length = self.get_article_length(requests.get(entry.link).text)
+        if article.length == 0:
+            article.length = len(article.title)
 
+        if 'author' in entry:
+            author = User(type=1)
+            author_splits = entry.author.split()
+            if len(author_splits) > 1:
+                author.first_name = author_splits[1]
+                author.last_name = author_splits[0]
+            if len(author_splits) == 1:
+                author.first_name = author_splits[0]
+                author.last_name = author_splits[0]
 
-def get_article_length(html):
-    matches = re.findall('"articleLength":([0-9]+),', html)
-    if len(matches) > 0:
-        return int(matches[0])
-    else:
-        return 0
+            article.author_id = self.db.save_user(author)
 
+        self.db.save_article(article)
 
-def main():
-    db_user = os.environ.get("DB_USER")
-    db_password = os.environ.get("DB_PASSWORD")
-    db = Database('localhost', db_user, db_password)
-
-    for (url, category) in feeds:
-        feed = feedparser.parse(url)
-        for entry in feed.entries:
-            title = entry.title
-            published_at = entry.published_parsed
-            article_id_splits = entry.id.split("-")
-            article_id = article_id_splits[-1].split(".")[0][2:]
-
-            html = get_detail_html(entry.link)
-            comment_count = get_comment_count(article_id)
-            article_length = get_article_length(html)
-
-            author_id = None
-            if 'author' in entry:
-                author_splits = entry.author.split()
-                if len(author_splits) > 1:
-                    author_id = db.save_author(
-                        author_splits[0], author_splits[1])
-                if len(author_splits) == 1:
-                    author_id = db.save_author(
-                        author_splits[0], author_splits[0])
-
-            category_id = db.save_category(category)
-
-            db.save_article(article_id, author_id, 2, title,
-                            article_length, comment_count, published_at, category_id)
+    def process_feed(self):
+        for (url, category) in feeds:
+            self.process_rss_feed(url, self.process_entry, category)
 
 
 if __name__ == '__main__':
-    # Configure SSL to not validate certificates
-    ssl._create_default_https_context = ssl._create_unverified_context
-    main()
+    Blick().process_feed()
